143c7a9a8c4bbf328c18907e0d6b3bb9c17d1b33
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-05-24 11:44:27 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isCycle(vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& recStack, int node){
+        visited[node] = true;
+        recStack[node] = true;;
+
+        for(int neighbour : adj[node]){
+            if(!visited[neighbour]){
+                if(isCycle(adj, visited, recStack, neighbour)) return true;
+            }
+            else if(recStack[neighbour]) return true;
+        }
+
+        recStack[node] = false;
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& preReqs) {
+        vector<vector<int>> adj(numCourses);
+        for(auto& e : preReqs) adj[e[1]].push_back(e[0]);
+
+        vector<bool> visited(numCourses, false);
+        vector<bool> recStack(numCourses, false);
+
+        for(int i = 0; i < numCourses; i++){
+            if(!visited[i]){
+                if(isCycle(adj, visited, recStack, i)) return false;
+            }
+        }
+
+        return true;
+    }
+};
